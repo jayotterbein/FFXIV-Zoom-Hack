88ad1cf83d8a14f091737dc16180b62a0e9a03f6
kingsznhone <35349300+kingsznhone@users.noreply.github.com>
2022-01-23 16:35:45 +0800

    Zoom Hack Ultimate
    No more offset update shit.
    One version forever.

--- FFXIVZoomHack/Settings.cs
+++ FFXIVZoomHack/Settings.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 using System.Windows.Forms;
-using System.Xml.Linq;
 
 namespace FFXIVZoomHack
 {
@@ -16,133 +14,53 @@ namespace FFXIVZoomHack
         {
             AutoApply = true;
             AutoQuit = false;
-
             DesiredZoom = 20;
             DesiredFov = 0.78f;
-
-            OffsetUpdateLocation = @"https://raw.githubusercontent.com/jayotterbein/FFXIV-Zoom-Hack/master/Offsets.xml";
-            LastUpdate = "unupdated";
         }
 
-        private static string SettingsFile
-        {
-            get
-            {
-                var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FFXIVZoomHack");
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-                return Path.Combine(path, "Settings.xml");
-            }
-        }
-
+        
         public bool AutoApply { get; set; }
         public bool AutoQuit { get; set; }
         public float DesiredFov { get; set; }
         public float DesiredZoom { get; set; }
 
-        public int[] DX11_StructureAddress { get; set; }
-        public int DX11_ZoomCurrent { get; set; }
-        public int DX11_ZoomMax { get; set; }
-        public int DX11_FovCurrent { get; set; }
-        public int DX11_FovMax { get; set; }
+        private static string SettingsPath
+        {
+            get
+            {
+                var path = new FileInfo(Application.ExecutablePath).Directory.ToString() + @"\Setting.json";
+                if (!File.Exists(path))
+                {
+                    Settings settings = new Settings();
+                    settings.Save();
+                }
+                return path;
+            }
+        }
 
-        public string OffsetUpdateLocation { get; set; }
-        public string LastUpdate { get; set; }
-
-        public static Settings Load(string location = null)
+        public static Settings Load()
         {
             var settings = new Settings();
             try
             {
-                var doc = XDocument.Load(location ?? SettingsFile);
-                var root = doc.Element("Root");
-                foreach (var element in root.Elements())
-                {
-                    switch (element.Name.LocalName)
-                    {
-                        case "AutoApply":
-                            settings.AutoApply = bool.Parse(element.Value);
-                            break;
-                        case "AutoQuit":
-                            settings.AutoQuit = bool.Parse(element.Value);
-                            break;
-                        case "DX11":
-                            settings.DX11_StructureAddress = element.Element("StructureAddress")
-                                .Value
-                                .Split(',')
-                                .Where(x => !string.IsNullOrEmpty(x))
-                                .Select(x => int.Parse(x, NumberStyles.HexNumber, CultureInfo.InvariantCulture))
-                                .ToArray();
-                            settings.DX11_ZoomCurrent = int.Parse(element.Element("ZoomCurrent").Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-                            settings.DX11_ZoomMax = int.Parse(element.Element("ZoomMax").Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-                            settings.DX11_FovCurrent = int.Parse(element.Element("FovCurrent").Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-                            settings.DX11_FovMax = int.Parse(element.Element("FovMax").Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-                            break;
-                        case "DesiredZoom":
-                            settings.DesiredZoom = float.Parse(element.Value, CultureInfo.InvariantCulture);
-                            if (settings.DesiredZoom < 1f || settings.DesiredZoom > 1000f)
-                            {
-                                settings.DesiredZoom = 20f;
-                            }
-                            break;
-                        case "DesiredFov":
-                            settings.DesiredFov = float.Parse(element.Value, CultureInfo.InvariantCulture);
-                            if (settings.DesiredFov < 0.01f || settings.DesiredFov > 3f)
-                            {
-                                settings.DesiredFov = 0.78f;
-                            }
-                            break;
-                        case "LastUpdate":
-                            settings.LastUpdate = element.Value;
-                            break;
-                    }
-                }
+                settings = JsonSerializer.Deserialize<Settings>(SettingsPath);
             }
-            catch
-            {
-                /* xml failed to load, lose settings */
-            }
+            catch { }
             return settings;
         }
 
+
         public void Save()
         {
-            var doc = new XDocument(
-                new XDeclaration("1.0", Encoding.UTF8.ToString(), "yes"),
-                new XElement("Root",
-                    GetSaveElements().ToArray<object>()
-                    )
-                );
-
-            using (var fstream = new FileStream(SettingsFile, FileMode.Create, FileAccess.Write))
-            using (var sw = new StreamWriter(fstream, Encoding.UTF8))
+            var options = new JsonSerializerOptions
             {
-                doc.Save(sw, SaveOptions.None);
-            }
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                WriteIndented = true
+            };
+            string JsonText = JsonSerializer.Serialize(this, options);
+
+            File.WriteAllText(SettingsPath, JsonText);
         }
 
-        private IEnumerable<XElement> GetSaveElements()
-        {
-            if (DX11_StructureAddress?.Any() == true)
-            {
-                yield return new XElement("DX11",
-                    new XElement("StructureAddress",
-                        string.Join(",", DX11_StructureAddress.Select(x => x.ToString("X", CultureInfo.InvariantCulture)))),
-                    new XElement("ZoomCurrent", DX11_ZoomCurrent.ToString("X", CultureInfo.InvariantCulture)),
-                    new XElement("ZoomMax", DX11_ZoomMax.ToString("X", CultureInfo.InvariantCulture)),
-                    new XElement("FovCurrent", DX11_FovCurrent.ToString("X", CultureInfo.InvariantCulture)),
-                    new XElement("FovMax", DX11_FovMax.ToString("X", CultureInfo.InvariantCulture))
-                );
-            }
-
-            yield return new XElement("LastUpdate", LastUpdate);
-            yield return new XElement("AutoApply", AutoApply.ToString(CultureInfo.InvariantCulture));
-            yield return new XElement("AutoQuit", AutoQuit.ToString(CultureInfo.InvariantCulture));
-            yield return new XElement("DesiredZoom", DesiredZoom.ToString(CultureInfo.InvariantCulture));
-            yield return new XElement("DesiredFov", DesiredFov.ToString(CultureInfo.InvariantCulture));
-            yield return new XElement("OffsetUpdateLocation", OffsetUpdateLocation);
-        }
     }
 }
